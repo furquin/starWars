afc910e1f52762bc791660fb643c3b618c5c1ae6
furquin <laert.ff@gmail.com>
2022-01-25 14:43:06 -0300

    feat: cria componente table

---
+++ src/components/Table/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>name</th>
+          <th>rotation_period</th>
+          <th>orbital_period</th>
+          <th>diameter</th>
+          <th>climate</th>
+          <th>gravity</th>
+          <th>terrain</th>
+          <th>surface_water</th>
+          <th>population</th>
+          <th>residents</th>
+          <th>films</th>
+          <th>created</th>
+          <th>edited</th>
+          <th>url</th>
+        </thead>
+      </table>
+    </div>
+  );
+}
+export default Table;
