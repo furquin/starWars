b8c50b9ed4cfb7e928e5ec80d00b02da06f34277
furquin <laert.ff@gmail.com>
2022-01-27 10:09:04 -0300

    fix: realiza map para excluir key residents

--- src/service/fetchAPI.js
+++ src/service/fetchAPI.js
@@ -3,7 +3,11 @@ const fetchApi = async () => {
     'https://swapi-trybe.herokuapp.com/api/planets/',
   )
     .then((data) => data.json())
-    .then((data) => data.results);
+    .then((data) => data.results)
+    .then((data) => data.map((item) => {
+      delete item.residents;
+      return item;
+    }));
   return response;
 };
 
