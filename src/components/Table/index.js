242fd5d064387a4340e62fa809ea5bb05e0299b4
furquin <laert.ff@gmail.com>
2022-01-27 10:06:53 -0300

    feat: realiza filtro atraves do nome

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -39,24 +39,26 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {data
-            && data.map((result) => (
-              <tr key={ result.name }>
-                <td>{result.name}</td>
-                <td>{result.rotation_period}</td>
-                <td>{result.orbital_period}</td>
-                <td>{result.diameter}</td>
-                <td>{result.climate}</td>
-                <td>{result.gravity}</td>
-                <td>{result.terrain}</td>
-                <td>{result.surface_water}</td>
-                <td>{result.population}</td>
-                <td>{result.films}</td>
-                <td>{result.created}</td>
-                <td>{result.edited}</td>
-                <td>{result.url}</td>
-              </tr>
-            ))}
+          {data.length > 0
+            && data
+              .filter((planet) => planet.name.includes(filterName.filterByName.name))
+              .map((result) => (
+                <tr key={ result.name }>
+                  <td>{result.name}</td>
+                  <td>{result.rotation_period}</td>
+                  <td>{result.orbital_period}</td>
+                  <td>{result.diameter}</td>
+                  <td>{result.climate}</td>
+                  <td>{result.gravity}</td>
+                  <td>{result.terrain}</td>
+                  <td>{result.surface_water}</td>
+                  <td>{result.population}</td>
+                  <td>{result.films}</td>
+                  <td>{result.created}</td>
+                  <td>{result.edited}</td>
+                  <td>{result.url}</td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
