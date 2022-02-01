e109175ef83c4acc676c94374adceb9e617d3704
furquin <laert.ff@gmail.com>
2022-01-31 23:19:22 -0300

    feat: renderiza input para digitar nome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FilterByName from './components/Filters/FilterByName';
 import Table from './components/Table';
 import GlobalProvider from './Context/GlobalProvider';
 
 export default function App() {
   return (
     <GlobalProvider>
+      <FilterByName />
       <Table />
     </GlobalProvider>);
 }
