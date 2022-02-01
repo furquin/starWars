89e7bc7a34ccc81f07a3af0bcc658cf6f6e3f466
furquin <laert.ff@gmail.com>
2022-02-01 00:11:41 -0300

    feat: renderiza filtros na pagina '

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import FilterByName from './components/Filters/FilterByName';
+import FilterByValues from './components/Filters/FilterByValues';
 import Table from './components/Table';
 import GlobalProvider from './Context/GlobalProvider';
 
 export default function App() {
   return (
     <GlobalProvider>
+
       <FilterByName />
+      <FilterByValues />
       <Table />
     </GlobalProvider>);
 }
