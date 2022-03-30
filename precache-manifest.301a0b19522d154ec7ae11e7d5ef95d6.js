f9f4fce35948da6530939b348f171c2f9146a17f
furquin <laert.ff@gmail.com>
2022-03-29 22:18:26 -0300

    Updates

---
+++ precache-manifest.301a0b19522d154ec7ae11e7d5ef95d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a261f9c20c9517e1f42a12ef27401ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "291cef03080aae8edc8e",
+    "url": "./static/css/main.69f0574e.chunk.css"
+  },
+  {
+    "revision": "d04b2a40c4630715ce2c",
+    "url": "./static/js/2.922117fc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.922117fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291cef03080aae8edc8e",
+    "url": "./static/js/main.ce5d89e0.chunk.js"
+  },
+  {
+    "revision": "1e503d3d0e04877e2d1d1b6a2f74ddcc",
+    "url": "./static/js/main.ce5d89e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09157014376cf56a2e0d",
+    "url": "./static/js/runtime-main.f33c1431.js"
+  }
+]);
